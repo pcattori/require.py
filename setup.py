6310e84c2cf830446e470d878553960ca805afc3
Pedro Cattori <pcattori@gmail.com>
2016-08-13 00:44:46 -0400

    setup.py description matches repo description on github

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='require.py',
   version='1.0.0',
-  description='Relative imports inspired by require.js',
+  description='Relative imports in Python, inspired by Node.js `require`',
   url='https://github.com/pcattori/require-py',
   author='Pedro Cattori',
   author_email='pcattori@gmail.com',
