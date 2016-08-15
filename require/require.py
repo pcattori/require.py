3f27fc74cc08ab2315b2c0460b5b62ee839c8f13
Pedro Cattori <pcattori@gmail.com>
2016-08-14 23:02:11 -0400

    fixes #3 paths starting with '/' are treated as absolute paths

--- require/require.py
+++ require/require.py
@@ -7,14 +7,17 @@ import os
 # require.py takes advantage of this to namespace its modules
 PREFIX = '@require-'
 
-def py(relative_path):
+def py(path):
   '''Returns the module object for the module at the specified relative path.
   Path is relative to calling script's path.
   '''
-  # get absolute path by rooting relative path with calling script directory
-  caller_relative_filepath = inspect.stack()[1][1]
-  caller_root = os.path.dirname(os.path.abspath(caller_relative_filepath))
-  absolute_path = os.path.abspath(os.path.join(caller_root, relative_path))
+  if path.startswith('/'):
+    absolute_path = path
+  else:
+    # get absolute path by rooting relative path with calling script directory
+    caller_relative_filepath = inspect.stack()[1][1]
+    caller_root = os.path.dirname(os.path.abspath(caller_relative_filepath))
+    absolute_path = os.path.abspath(os.path.join(caller_root, path))
 
   # compute directory path and filename without extension
   dirpath, filename = os.path.split(absolute_path)
