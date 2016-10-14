a07299bb6b426604aa32b6efcc5e497e98235fe8
Pedro Cattori <pcattori@gmail.com>
2016-10-14 18:04:41 -0400

    version bump for name change and python3 support

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
 setup(
-  name='require.py',
-  version='1.1.1',
-  description='Path-based imports in Python, inspired by Node.js `require`',
-  url='https://github.com/pcattori/require-py',
+  name='require',
+  version='2.0.0',
+  description="Path-based imports in Python, inspired by `require.js`",
+  url='https://github.com/pcattori/require.py',
   author='Pedro Cattori',
   author_email='pcattori@gmail.com',
   license='MIT',
   packages=['require'],
+  install_requires=[
+    'six>=1.10.0'
+  ],
   test_suite='tests'
 )
