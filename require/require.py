c9556e4438c6d55d2af717f44eaf3b920be76fb3
Pedro Cattori <pcattori@gmail.com>
2016-10-14 14:08:02 -0400

    python3 require

--- require/require.py
+++ require/require.py
@@ -1,31 +1,64 @@
-from __future__ import unicode_literals
-import imp
+import importlib.util
 import inspect
 import os
 
-# normally python module names cannot contain symbols like '@'
-# require.py takes advantage of this to namespace its modules
-PREFIX = '@require-'
+# TODO load more than 1x?
+def load_module_at(absolute_path):
+    '''Loads Python module at specified absolute path.
 
-def py(path):
-  '''Returns the module object for the module at the specified path.
-  If path begins with a '/', it is treated as an absolute path.
-  Otherwise, the path is considered relative to calling script's path.
-  '''
-  if os.path.isabs(path):
-    absolute_path = path
-  else:
-    # get absolute path by rooting relative path with calling script directory
-    caller_relative_filepath = inspect.stack()[1][1]
+    :param str absolute_path: Absolute path to the desired Python module.
+    :return: Imported Python module
+    :rtype: Module
+
+    Usage::
+      >>> import require
+      >>> require.load_module_at('/absolute/path/to/module')
+      Module
+    '''
+    if os.path.isdir(absolute_path):
+        absolute_path = os.path.join(absolute_path, '__init__.py')
+
+    if not os.path.exists(absolute_path):
+        raise ImportError('No module at {}'.format(absolute_path))
+    spec = importlib.util.spec_from_file_location(absolute_path, absolute_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+def resolve_path(path, upstack=0):
+    '''Resolve a path to an absolute path by taking it to be relative to the source
+    code of the caller's stackframe shifted up by `upstack` frames.
+
+    :param str path: Filesystem path
+    :param int upstack: Number of stackframes upwards from caller's stackframe
+    to act as relative point.
+
+    #: TODO Usage example is not great on REPL...
+
+    Usage::
+      >>> import require # at /home/require
+      >>> require.resolve_path('file.txt')
+      '/home/require/file.txt'
+    '''
+    if os.path.isabs(path):
+        return path
+    # get absolute path by rooting path with calling script directory
+    caller_relative_filepath = inspect.stack()[upstack + 1][1]
     caller_root = os.path.dirname(os.path.abspath(caller_relative_filepath))
-    absolute_path = os.path.abspath(os.path.join(caller_root, path))
+    return os.path.abspath(os.path.join(caller_root, path))
 
-  # compute directory path and filename without extension
-  dirpath, filename = os.path.split(absolute_path)
-  filename_noext, _ = os.path.splitext(filename)
+# TODO python2/3 compatible?
+def require3(path):
+    '''
+    :param str path: Relative path to the desired Python module. Should be
+    relative to the path of the calling script.
 
-  # prefixed to avoid module namespace conflicts
-  module_name = PREFIX + filename_noext
+    Usage::
+      >>> import require # at /arbitrary/path
+      >>> foo = require.require3('./foo.py') # imports /arbitrary/path/foo.py
+      >>> foo
+      Module
+    '''
+    absolute_path = resolve_path(path, upstack=1)
+    return load_module_at(absolute_path)
 
-  spec = imp.find_module(filename_noext, [dirpath])
-  return imp.load_module(module_name, *spec)
