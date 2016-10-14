b4628139742a14d51fe9e45dcde99c969cf626dc
Pedro Cattori <pcattori@gmail.com>
2016-10-14 18:49:13 -0400

    classifiers

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setup(
     install_requires=[
         'six>=1.10.0'
     ],
-    test_suite='tests'
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ]
 )
